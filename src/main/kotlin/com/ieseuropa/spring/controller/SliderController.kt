8ba7ba816190783a05e57e9a05b39d41bce89bba
somaxa8 <silviofrancoxa8@gmail.com>
2021-01-22 15:45:48 -0400

    added slider

---
+++ src/main/kotlin/com/ieseuropa/spring/controller/SliderController.kt
@@ -0,0 +1,41 @@
+package com.ieseuropa.spring.controller
+
+import com.ieseuropa.spring.entity.Slider
+import com.ieseuropa.spring.service.SliderService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+class SliderController {
+
+    @Autowired lateinit var sliderService: SliderService
+
+
+    @PostMapping("/api/slider")
+    fun postSlider(
+            @RequestParam title: String,
+            @RequestParam imageFile: MultipartFile
+    ): ResponseEntity<Slider> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(sliderService.create(title, imageFile))
+    }
+
+    @DeleteMapping("/api/slider/{id}")
+    fun deleteSlider(@PathVariable id: Long): ResponseEntity<Void> {
+        sliderService.delete(id)
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null)
+    }
+
+    @GetMapping("/public/slider")
+    fun getSliders(): ResponseEntity<List<Slider>> {
+        return ResponseEntity.status(HttpStatus.OK).body(sliderService.findAll())
+    }
+
+    @GetMapping("/public/slider/{id}")
+    fun getSlider(@PathVariable id: Long): ResponseEntity<Slider> {
+        return ResponseEntity.status(HttpStatus.OK).body(sliderService.findById(id))
+    }
+
+}
---
+++ src/main/kotlin/com/ieseuropa/spring/service/SliderService.kt
@@ -0,0 +1,57 @@
+package com.ieseuropa.spring.service
+
+import com.ieseuropa.spring.config.exception.NotFoundException
+import com.ieseuropa.spring.entity.Document
+import com.ieseuropa.spring.entity.Slider
+import com.ieseuropa.spring.repository.SliderRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import javax.transaction.Transactional
+
+@Service
+@Transactional
+class SliderService {
+
+    @Autowired lateinit var sliderRepository: SliderRepository
+    @Autowired lateinit var documentService: DocumentService
+
+
+    fun init() {
+        if (sliderRepository.count() <= 0) {
+            TODO("IMPLEMENT THIS")
+        }
+    }
+
+    fun create(title: String, imageFile: MultipartFile): Slider {
+
+        val image = documentService.create(imageFile, Document.Type.IMAGE, Slider::class.java.simpleName, title)
+
+        val slider = Slider(
+                title = title,
+                image = image
+        )
+
+        return sliderRepository.save(slider)
+    }
+
+    fun findById(id: Long): Slider {
+        if (!sliderRepository.existsById(id)) {
+            throw NotFoundException()
+        }
+        return sliderRepository.getOne(id)
+    }
+
+    fun delete(id: Long) {
+        if (!sliderRepository.existsById(id)) {
+            throw NotFoundException()
+        }
+        val slider = findById(id)
+        slider.image?.let { documentService.delete(it.id!!) }
+        sliderRepository.deleteById(id)
+    }
+
+    fun findAll(): List<Slider> {
+        return sliderRepository.findAll()
+    }
+}
