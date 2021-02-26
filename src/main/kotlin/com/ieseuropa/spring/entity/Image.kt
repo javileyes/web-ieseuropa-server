533da3f7455ab962f697dbcf9931dafa921a5b66
somaxa8 <silviofrancoxa8@gmail.com>
2021-02-26 15:20:05 -0500

    fixed Image key property

--- src/main/kotlin/com/ieseuropa/spring/entity/Image.kt
+++ src/main/kotlin/com/ieseuropa/spring/entity/Image.kt
@@ -7,7 +7,8 @@ class Image(
         @Id @GeneratedValue
         var id: Long? = null,
         var title: String? = null,
-        var key: String?= null,
+        @Column(name = "key_")
+        var key: String? = null,
         @OneToOne
         var image: Document? = null
 )
